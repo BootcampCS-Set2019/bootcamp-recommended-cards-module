8bfb1831ad6fefde65dc515769515100bf34d3d9
Matheus Bispo <matheus.filipe.bispo@accenture.com>
2019-10-01 17:16:33 -0300

    Add initial project files

---
+++ RecommededCardsModule/RecommededCardsModule.h
@@ -0,0 +1,19 @@
+//
+//  RecommededCardsModule.h
+//  RecommededCardsModule
+//
+//  Created by matheus.filipe.bispo on 01/10/19.
+//  Copyright © 2019 matheus.filipe.bispo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RecommededCardsModule.
+FOUNDATION_EXPORT double RecommededCardsModuleVersionNumber;
+
+//! Project version string for RecommededCardsModule.
+FOUNDATION_EXPORT const unsigned char RecommededCardsModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecommededCardsModule/PublicHeader.h>
+
+
