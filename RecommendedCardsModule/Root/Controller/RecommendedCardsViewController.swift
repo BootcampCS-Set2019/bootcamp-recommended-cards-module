db9b37c3b9620e1c649b74b4594549f65c1edbf0
Matheus Bispo <matheus.filipe.bispo@accenture.com>
2019-10-09 13:37:24 -0300

    Remove RxCocoa import

--- RecommendedCardsModule/Root/Controller/RecommendedCardsViewController.swift
+++ RecommendedCardsModule/Root/Controller/RecommendedCardsViewController.swift
@@ -5,7 +5,6 @@
 //  Created by matheus.filipe.bispo on 06/10/19.
 //  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
 //
-import RxCocoa
 import Entities
 
 protocol RecommendedCardsDelegate: class {
